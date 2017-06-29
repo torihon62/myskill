ccf1fd3b6a7a9a941d6eec9b4cbd7e6cb8d436e7
torihon62 <torihon62@gmail.com>
2017-06-29 12:18:17 +0000

    グラフアプリ作成

--- app/controllers/demo_pages_controller.rb
+++ app/controllers/demo_pages_controller.rb
@@ -1,4 +1,7 @@
 class DemoPagesController < ApplicationController
   def demo1
   end
+
+  def demo2
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'demo_pages/demo1'
+  get 'demo_pages/demo2'
 
   resources :samples
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

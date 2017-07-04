7f5873e88e8c631445e5ec5b00deb0cea1e5d79c
torihon62 <torihon62@gmail.com>
2017-07-04 07:45:56 +0000

    font files precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # font files precompile
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/ 
 end
